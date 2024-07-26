9db7113b880d9d17cd65bac09a1ff612f2205642
Benson Manzano <bensonfmanzano@gmail.com>
2024-07-26 14:52:31 -0700

    Made some changes to the heights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Portfolio from "@/components/portfolio";
 export default function Home() {
   return (
     <main className="scroll-smooth bg-gradient-to-b from-gray-900 to-gray-600 min-h-dvh text-white">
-      <div className="flex flex-col min-h-dvh gap-80">
+      <div className="flex flex-col min-h-dvh gap-60">
         <Navbar />
         <Hero />
       </div>
