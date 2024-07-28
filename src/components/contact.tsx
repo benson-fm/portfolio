27b99ad19e6bb6c0638fa7994502b6592ed0c4ab
Benson Manzano <bfmanzan@uci.edu>
2024-07-27 22:09:44 -0700

    fixed unescaped entity

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -10,7 +10,7 @@ export default function Contact() {
   return (
     <>
       <div className="flex flex-col max-w-full h-full justify-center items-center gap-7">
-        <p className="text-4xl"> <span className="underline underline-offset-8">let's</span> connect!</p>
+        <p className="text-4xl"> <span className="underline underline-offset-8">let&apos;s</span> connect!</p>
         <IoMailOutline className="w-10 h-12 hover:cursor-pointer hover:animate-pulse" onClick={handleEmail}/>
       </div>
 
